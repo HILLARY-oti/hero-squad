71d3133459e1238f1ca0b7b789c36f443278d2bb
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-03 12:42:43 +0300

    add get method to the page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,19 @@
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args){
+        get("/herosquad", (request, response) ->
+                "<!DOCTYPE html>" +
+                        "<html>" +
+                        "<head>" +
+                        "<title>Hero Squad</title>" +
+                        "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
+                        "</head>" +
+                        "<body>" +
+                        "<h1>Hero Squad</h1>" +
+                        "<p>Who is Your Hero?</p/>" +
+                        "</body>" +
+                        "</html>"
+                );
+ }
 }
