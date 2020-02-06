2f567728409473cc315b97b4c1543a78e1202485
HILLARY-oti <hillaryotieno646@gmail.com>
2020-02-06 11:45:29 +0300

    add test files

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -1,7 +1,23 @@
 package models;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-class PostTest {
+import static org.junit.Assert.*;
 
+public class PostTest {
+
+    @Test
+    public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
+        Post post = new Post("hero", "superPower", "weakness", 12, "squad");
+        assertEquals(true, post instanceof Post);
+    }
+
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Rock","leaser eyes", "death", 18, "Joker squad");
+        assertEquals("hero,superPower,weakness,age,squad",post.getContent());
+
+    }
 }
